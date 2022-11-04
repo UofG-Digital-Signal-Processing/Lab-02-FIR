798864cbfc2922c6103aca20c2c1779076bda429
Chris <yuxiaohui0626@gmail.com>
2022-11-04 16:26:32 +0000

    class FirFilter

--- firfilter.py
+++ firfilter.py
@@ -1,6 +1,20 @@
-class FirFilter:
-    def __int__(self, _coefficients):
-        self.coefficients = _coefficients
+import numpy as np
 
-    def filter(self, v):
-        pass
+
+class FirFilter:
+    def __int__(self, _h):
+        self.h = _h
+        self.M = len(_h)
+        # Ring buffer
+        self.buffer = np.zeros(self.M)
+        self.offset = self.M - 1
+
+    def filter(self, input):
+        self.buffer[self.offset] = input
+        # Move the offset
+        self.offset -= 1
+        # Calculate the output
+        output = 0
+        for i in range(self.M):
+            output += self.h[i] * self.buffer[(i + self.offset) % self.M]
+        return output

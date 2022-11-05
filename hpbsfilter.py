04d3ee84bfe4aa9d28148102e5311d4cdd558c25
Chris <yuxiaohui0626@gmail.com>
2022-11-05 18:32:53 +0000

    Plot the result

--- hpbsfilter.py
+++ hpbsfilter.py
@@ -1,10 +1,9 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 import firdesign
+import util
 from firfilter import FirFilter
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     file_path = 'ECG_1000Hz.dat'
@@ -12,8 +11,8 @@ if __name__ == '__main__':
     band_stop_w_1 = 49
     band_stop_w_2 = 51
     high_pass_w_c = 2
-    data = np.loadtxt(file_path)
-    n = len(data)
+    original_data = np.loadtxt(file_path)
+    n = len(original_data)
     # Create bands-top filter
     band_stop_h = firdesign.band_stop_design(sample_rate, [band_stop_w_1, band_stop_w_2])
     band_stop_filter = FirFilter(band_stop_h)
@@ -23,68 +22,41 @@ if __name__ == '__main__':
     # Remove the 50Hz interference by the band-stop filter
     band_stop_output = np.zeros(n)
     for i in range(n):
-        band_stop_output[i] = band_stop_filter.do_filter(data[i])
+        band_stop_output[i] = band_stop_filter.do_filter(original_data[i])
     # Process the baseline wander by the high-pass filter
     high_pass_output = np.zeros(n)
     for i in range(n):
         high_pass_output[i] = high_pass_filter.do_filter(band_stop_output[i])
 
     # TODO Plot & Compare the results
-
-    # x1 = range(0,n)
-    # plt.figure(1)
-    # plt.subplot(2, 1, 1)
-    # plt.xlabel("time")
-    # plt.ylabel("Amplitude")
-    # plt.title("original_ECG")
-    # plt.plot(x1, data)
-    #
-    # plt.show()
-
-    amplitude = np.array(band_stop_output)
-
-    # calculating the total number of samples
-    total_samples = np.size(band_stop_output)
-
-    # calculating the time step between each sample
-    time_step = 1 / sample_rate
-
-    # calculating the time domain for the signal
-    time_domain = np.linspace(0, (total_samples - 1) * time_step, total_samples)
-
-    # calculating the frequency step size for the signal
-    freq_step = sample_rate / total_samples
-
-    # calculating the frequency domain for the signal
-    freq_domain = np.linspace(0, (total_samples - 1) * freq_step, total_samples)
-    freq_domain_plt = freq_domain[:int(total_samples / 2) + 1]
-
-    # calculating the frequency response of the signal
-    freq_mag = np.fft.fft(band_stop_output)
-    freq_mag_abs = np.abs(freq_mag) / total_samples
-    freq_mag_abs_plt = 2 * freq_mag_abs[:int(total_samples / 2) + 1]
-    freq_mag_dB = 20 * np.log10(freq_mag_abs_plt)
-
-    t = np.arange(0, n)
-    t = t / sample_rate
+    # Plot the original ECG time domain
+    original_time, original_amplitude = util.cal_time_domain(original_data, sample_rate)
     plt.subplot(2, 2, 1)
-    plt.title("Original signal")
-    plt.plot(t, data)
-
+    plt.plot(original_time, original_amplitude)
+    plt.title('Original ECG Time Domain')
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    # Plot the original ECG frequency domain
+    original_freq, original_amplitude = util.cal_frequency_domain(original_data, sample_rate)
     plt.subplot(2, 2, 2)
-    plt.title("After eliminating baseline wander")
-    plt.plot(t, high_pass_output)
-
+    plt.plot(original_freq, original_amplitude)
+    plt.title('Original ECG Frequency Domain')
+    plt.xlabel("Frequency")
+    plt.ylabel("Amplitude")
+    # Plot the filtered ECG time domain
+    filtered_time, filtered_amplitude = util.cal_time_domain(high_pass_output, sample_rate)
     plt.subplot(2, 2, 3)
-    plt.title("After eliminating 50Hz")
-    plt.plot(t, band_stop_output)
-
+    plt.plot(filtered_time, filtered_amplitude)
+    plt.title('Filtered ECG Time Domain')
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    # Plot the filtered ECG frequency domain
+    filtered_freq, filtered_amplitude = util.cal_frequency_domain(high_pass_output, sample_rate)
     plt.subplot(2, 2, 4)
-    plt.title("After eliminating 50Hz")
-    plt.xlabel("frequency")
-    plt.ylabel("dB")
-    plt.plot(freq_domain_plt, freq_mag_abs_plt)
-
+    plt.plot(filtered_freq, filtered_amplitude)
+    plt.title('Filtered ECG Frequency Domain')
+    plt.xlabel("Frequency")
+    plt.ylabel("Amplitude")
     plt.tight_layout()
+    plt.savefig("res/task_2.svg")
     plt.show()
-
--- lmsfilter.py
+++ lmsfilter.py
@@ -1,8 +1,9 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 import firdesign
+import util
 from firfilter import FirFilter
-import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     file_path = 'ECG_1000Hz.dat'
@@ -11,8 +12,8 @@ if __name__ == '__main__':
     noise_frequency = 50
     learning_rate = 0.001
     high_pass_w_c = 2
-    data = np.loadtxt(file_path)
-    n = len(data)
+    original_data = np.loadtxt(file_path)
+    n = len(original_data)
     # Create LMS filter
     lms_filter = FirFilter(np.zeros(int(sample_rate / frequency_resolution)))
     # Create high-pass filter
@@ -22,22 +23,40 @@ if __name__ == '__main__':
     lms_output = np.zeros(n)
     for i in range(n):
         noise = np.sin(2.0 * np.pi * noise_frequency / sample_rate * i)
-        lms_output[i] = lms_filter.do_filter_adaptive(data[i], noise, learning_rate)
-        #print(lms_output[i])
+        lms_output[i] = lms_filter.do_filter_adaptive(original_data[i], noise, learning_rate)
     # Process the baseline wander by the high-pass filter
     high_pass_output = np.zeros(n)
     for i in range(n):
         high_pass_output[i] = high_pass_filter.do_filter(lms_output[i])
 
-    # TODO Plot & Compare the results
-    t = np.arange(0, n)
-    t = t / sample_rate
-    plt.figure(1)
-    plt.title("original singal")
-    plt.plot(t, data)
-
-    plt.figure(2)
-    plt.title("Filter by Adaptive LMS")
-    plt.plot(t, high_pass_output)
-
+    # Plot the original ECG time domain
+    original_time, original_amplitude = util.cal_time_domain(original_data, sample_rate)
+    plt.subplot(2, 2, 1)
+    plt.plot(original_time, original_amplitude)
+    plt.title('Original ECG Time Domain')
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    # Plot the original ECG frequency domain
+    original_freq, original_amplitude = util.cal_frequency_domain(original_data, sample_rate)
+    plt.subplot(2, 2, 2)
+    plt.plot(original_freq, original_amplitude)
+    plt.title('Original ECG Frequency Domain')
+    plt.xlabel("Frequency")
+    plt.ylabel("Amplitude")
+    # Plot the filtered ECG time domain
+    filtered_time, filtered_amplitude = util.cal_time_domain(high_pass_output, sample_rate)
+    plt.subplot(2, 2, 3)
+    plt.plot(filtered_time, filtered_amplitude)
+    plt.title('Filtered ECG Time Domain')
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    # Plot the filtered ECG frequency domain
+    filtered_freq, filtered_amplitude = util.cal_frequency_domain(high_pass_output, sample_rate)
+    plt.subplot(2, 2, 4)
+    plt.plot(filtered_freq, filtered_amplitude)
+    plt.title('Filtered ECG Frequency Domain')
+    plt.xlabel("Frequency")
+    plt.ylabel("Amplitude")
+    plt.tight_layout()
+    plt.savefig("res/task_3.svg")
     plt.show()
